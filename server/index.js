2f374c0ba5a49f12638e7ebdfb2057b647dc8402
jeffigy <ejeffydev@gmail.com>
2023-10-31 07:08:08 +0800

    added CORS

--- server/index.js
+++ server/index.js
@@ -3,8 +3,18 @@ import { PORT, mongoDBURL } from "./config.js";
 import mongoose from "mongoose";
 const app = express();
 import booksRoute from "./routes/booksRoute.js";
+import cors from "cors";
+
 //middleware for parsing request body
 app.use(express.json());
+app.use(cors()); // allow all origins
+// app.use(
+//   cors({
+//     origin: "http://localhost:3000",
+//     methods: ["GET", "POST", "PUT", "DELETE"],
+//     allowedHeaders: ["Content-Type"],
+//   })
+// ); // 2nd method allow all origins
 
 app.get("/", (req, res) => {
   console.log(req);
